da8a6c3bb677f8dd0f33e49918da7bc50d8b60c9
mingle08 <mingle08@163.com>
2018-10-27 23:27:30 +0800

    新增二个服务提供者：8002，8003

--- sts/microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/sxt/springcloud/cfgbeans/ConfigBean.java
+++ sts/microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/sxt/springcloud/cfgbeans/ConfigBean.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+
 @Configuration
 public class ConfigBean {
 
@@ -12,6 +15,11 @@ public class ConfigBean {
 	@LoadBalanced  //Spring Cloud Ribbon是基于Netflix Ribbon实现的一套客户端 负载均衡的工具。
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+	
+	@Bean
+	public IRule myRule() {
+		return new RandomRule();  // //达到的目的，用我们重新选择的随机算法替代默认的轮询。
 	}
 	
 }
