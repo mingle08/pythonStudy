59f7a20573da17adce2382c61e6424bd95740c3b
mingle08 <mingle08@163.com>
2018-10-28 12:04:38 +0800

    自定义一个负载均衡策略：每台服务器轮询调用5次

--- sts/microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/sxt/myrule/MySelfRule.java
+++ sts/microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/sxt/myrule/MySelfRule.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RoundRobinRule;
 
 /**
  * 这个类，不能与主启动类在同一个包，也不能在主启动类所在包的子包
@@ -16,6 +15,7 @@ public class MySelfRule {
 
 	@Bean
 	public IRule myRule() {
-		return new RoundRobinRule();
+//		return new RoundRobinRule();
+		return new RandomRule_HXM();
 	}
 }
