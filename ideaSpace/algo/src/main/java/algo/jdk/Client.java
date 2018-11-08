9060171f7f6f6223dc7b02bad91a8f561120aea3
mingle08 <mingle08@163.com>
2018-11-08 17:29:51 +0800

    状态模式

---
+++ ideaSpace/algo/src/main/java/algo/jdk/Client.java
@@ -0,0 +1,16 @@
+package study.statePattern;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Context context = new Context();
+        context.setLiftState(new ClosingState());
+        context.open();
+        context.close();
+        context.run();
+        context.stop();
+        
+
+    }
+
+}
