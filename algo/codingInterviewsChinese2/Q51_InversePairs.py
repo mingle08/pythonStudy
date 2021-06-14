b132a140cbba8796e1724dce56dbd10f1b3f4e76
mingle08 <mingle08@163.com>
2021-06-14 11:25:50 +0800

    剑指offer 51: 逆序对

---
+++ algo/codingInterviewsChinese2/Q51_InversePairs.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        def mergeSort(L, R):
+            if L >= R:
+                return 0
+            m = L + (R - L) / 2
+            cnt = mergeSort(L, m) + mergeSort(m + 1, R)
+            i, j = L, m + 1
+            pos = L
+            while i <= m and j <= R:
+                if nums[i] <= nums[j]:
+                    tmp[pos] = nums[i]
+                    i += 1
+                else:
+                    tmp[pos] = nums[j]
+                    j += 1
+                    cnt += (m - i + 1)
+                pos += 1
+            for k in range(i, m + 1):
+                tmp[pos] = nums[k]
+                pos += 1
+            for k in range(j, R + 1):
+                tmp[pos] = nums[k]
+                pos += 1
+            nums[L:R + 1] = tmp[L:R + 1]
+            return cnt
+
+        n = len(nums)
+        tmp = [0] * n
+        return mergeSort(0, n - 1)
